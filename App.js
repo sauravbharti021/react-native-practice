9c3692d3f0f70df63f4572ca110ad3e8da877ee5
saurav <sauravbhartibgs37@gmail.com>
2024-05-19 09:03:54 +0530

    refactored App.js

--- App.js
+++ App.js
@@ -37,42 +37,6 @@ import RegisterScreen from "./screens/RegisterScreen";
 import ListingItemScreen from "./screens/ListingItemScreen";
 
 export default function App() {
-  const orientation = useDeviceOrientation();
-  console.log(orientation);
-
-  const [initial, setInitial] = useState(false);
-  const changeColor = () => {
-    Alert.prompt("Enter your desired color", "type in lowercase", [
-      {
-        text: "Confirm",
-        onPress: () => {
-          setInitial(!initial);
-          console.log("haha so funny");
-        },
-      },
-    ]);
-  };
-
-  const handlePress = () => {
-    console.log("pressed text, ...");
-  };
-
-  const categories = [
-    {
-      label: "Furniture",
-      value: 1,
-    },
-    {
-      label: "Clothing",
-      value: 2,
-    },
-    {
-      label: "Camera",
-      value: 3,
-    },
-  ];
-
-  const [category, setCategory] = useState();
 
   return (
     <>
@@ -90,7 +54,7 @@ export default function App() {
         />
         <AppTextInput icon="email" placeholder="Email" />
       </Screen> */}
-      
+    
       {/* <ProfileComponent title="Mosh Bhai" subTitle="Moshbhai123@gmail.com" /> */}
       {/* <MessagesScreen /> */}
       {/* <ListingDetailsScreen /> */}
@@ -98,83 +62,8 @@ export default function App() {
       {/* <ButtonComponent title={"Login"} onPress={() => {console.log("tapped")}} />  */}
       {/* <WelcomeScreen /> */}
       {/* <ViewImageScreen />  */}
-      {/* <View
-        style={{
-          width: 100,
-          height: 100,
-          backgroundColor: "royalblue",
-          borderColor: "blue",
-          borderRadius: 10,
-          position: "absolute",
-          top: "50%",
-          left: "40%",
-          // borderWidth: 10,
-          shadow: "lightgreen",
-          shadowOffset: { width: 10, height: 10 },
-          shadowOpacity: 0.5,
-          shadowRadius: 5,
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: "tomato",
-            width: 50,
-            height: 50,
-          }}
-        ></View>
-      </View>
 
-      <Text
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          position: "absolute",
-          top: "45%",
-          width: '100%',
-          color: 'tomato',
-          textTransform: "capitalize",
-          textAlign: 'center'
-        }}
-      >
-        <AppText>
-          React Native is Nice{" "}
-        </AppText>
-      </Text>
-      <View style={{flex:1, flexDirection: 'row', position: 'absolute', bottom: 20, width: '100%', justifyContent: 'space-evenly' }}>
-        <MaterialCommunityIcons name="email" size={40} />
-        <MaterialCommunityIcons name="instagram" size={40} />
-        <MaterialCommunityIcons name="twitter" size={40} />
-      </View> */}
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: '#fff',
-    backgroundColor: "lightgreen",
-    // alignItems: 'center',
-    // justifyContent: 'center',
-    backgroundColor: Platform.OS == "ios" ? "cyan" : "lightgreen",
-    paddingTop: Platform.OS == "ios" ? StatusBar.currentHeight : 10,
-  },
-  body: {
-    flex: 1,
-    backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
-const imageStyle = StyleSheet.create({
-  image: {
-    width: 300,
-    height: 200,
-    resizeMode: "contain",
-  },
-});
-const dimensionHelper = {
-  width: "100%",
-  height: 50,
-};
