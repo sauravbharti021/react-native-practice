616bc2db1f27c8f04333f3bd47968914d485f1ed
saurav <sauravbhartibgs37@gmail.com>
2024-05-20 08:26:58 +0530

    Added auth navigation

--- App.js
+++ App.js
@@ -38,23 +38,124 @@ import RegisterScreen from "./screens/RegisterScreen";
 import ListingItemScreen from "./screens/ListingItemScreen";
 import ImageInputList from "./components/ImageInput/ImageInputList";
 
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import AuthNavigator from "./navigation/AuthNavigator";
+
+const Link = ({}) => {
+  const navigation = useNavigation()
+  return (
+    <ButtonComponent 
+      title="Click"
+      onPress={() => {
+        navigation.navigate('TweetDetails', {id: 2, message: "2"})
+      }}
+      color=  'dodgerblue'
+    />
+  )
+}
+
+const Tweets = ({navigation}) => {
+  return (
+    <Screen>
+      <Text>
+        Tweets
+      </Text>
+      {/* <ButtonComponent color="dodgerblue" title="View Tweet" onPress={() => {
+        navigation.navigate("TweetDetails")
+      }} /> */}
+      <Link />
+    </Screen>
+  )
+}
+
+const TweetDetails = ({route}) => {
+  return (
+    <Screen>
+      <Text style={{color: 'red'}}>
+        Tweet Details {route.params.id}
+      </Text>
+    </Screen>
+  )
+}
+const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
+
+const StackNavigator= () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: {backgroundColor: 'tomato'},
+        headerTintColor: 'yellow'
+    }}
+  >
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} 
+      options={{
+        
+      }} 
+    />
+  </Stack.Navigator>
+)
+
+const Account = ()=> {
+  return (
+    <Screen>
+      <Text>
+        Account
+      </Text>
+    </Screen>
+  )
+}
+const Feed = ()=> {
+  return (
+    <Screen>
+      <Text>
+        Feed
+      </Text>
+    </Screen>
+  )
+}
+
+const TabNavigator = () => {
+  return(
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveBackgroundColor: "tomato",
+        tabBarActiveTintColor: "white",
+        // tabBarInactiveBackgroundColor: 'green'
+
+      }}
+    >
+      <Tab.Screen name="Feed" component={Feed} 
+        options={{
+          tabBarIcon: ({size, color}) => <MaterialCommunityIcons name="tab" size={size} color={color}  />
+        }}
+      />
+      <Tab.Screen name="Account" component={Account} 
+        options={{
+          tabBarIcon: ({size, color}) => <MaterialCommunityIcons name="home" size={size} color={color} />
+        }}
+      />
+    </Tab.Navigator>
+
+  )
+}
+
 export default function App() {
-  const [imageUris, setImageUris] = useState([]);
 
   return (
     <>
-      <Screen>
-        {/* <ImageInputList
-          imageUris={imageUris}
-          onAddImage={(uri) => {
-            setImageUris([...imageUris, uri]);
-          }}
-          onRemoveImage={(uri)=> {
-            setImageUris( (prevState) => prevState.filter((u) => uri!==u))
-          }}
-        /> */}
-      <ListingItemScreen />
-      </Screen>
+
+      <NavigationContainer>
+        {/* <StackNavigator /> */}
+        {/* <TabNavigator /> */}
+        <AuthNavigator />
+      </NavigationContainer>
+      {/* <Screen>
+
+      </Screen> */}
+      {/* <ListingItemScreen /> */}
       {/* <RegisterScreen /> */}
       {/* <LoginScreen /> */}
       {/* <Screen>
